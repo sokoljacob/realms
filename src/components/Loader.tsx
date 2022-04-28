163a8e42b1dcfdb96df20bba71c185176b13cd52
sokoljacob <103051743+sokoljacob@users.noreply.github.com>
2022-04-28 10:55:10 -0500

    Update Loader.tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -7,7 +7,8 @@ type Props = {
 export const Loader: FC<Props> = ({ text = "Loading...", noText = false }) => {
   return (
     <div className="flex flex-col justify-center items-center text-xl font-light">
-      <svg
+      <img src="/looky.gif" />
+      {/* <svg
         className="animate-spin h-8 w-8 text-white"
         xmlns="http://www.w3.org/2000/svg"
         fill="white"
@@ -26,7 +27,7 @@ export const Loader: FC<Props> = ({ text = "Loading...", noText = false }) => {
           fill="currentColor"
           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
         ></path>
-      </svg>{" "}
+      </svg>{" "} */}
       {!noText ? <div className="opacity-50 mt-4">{text}</div> : null}
     </div>
   );
